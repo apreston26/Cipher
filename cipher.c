070b0e3035e6b5389a976e1616c9936390e0eb1b
Austin Preston <austinpreston314@gmail.com>
2021-11-07 14:45:35 -0700

    Handles first line

--- cipher.c
+++ cipher.c
@@ -7,20 +7,20 @@
 #define DFLAG 2
 
 void characterHandler();
-void cipherSolver(int m, int c, int flag, int b);
+int cipherSolver(int m, int c, int flag, int b);
 
 int main()
 {
-  /*characterHandler();*/
-  cipherSolver(126, 25, 1, 2); 
+  characterHandler();
   return 0;
 }
-/*
+
 void characterHandler()
 {
   int chars = 0;
   int isLine = 0;
   int lineCounter = 1;
+  int dataCounter = 0;
   int flag = OFF;
   int errorFlag = OFF;
   int lcg_m = 0;
@@ -41,6 +41,7 @@ void characterHandler()
 	  if (lcg_m == 0) lcg_m = value;
 	  if (lcg_m != 0) lcg_c = value;
 	  value = 0;
+	  dataCounter++;
 	}
       if (isLine == 0)
         {
@@ -49,50 +50,50 @@ void characterHandler()
 	  if (chars == 'd') flag = DFLAG;
           isLine = 1;
         }
+      if (dataCounter == 2 && chars != ',')
+	{
+	  printf("%d\n", cipherSolver(lcg_m, lcg_c, flag, chars));
+	}
       if (chars == '\n')
         {
+	  lcg_m = 0;
+	  lcg_c = 0;
+	  value = 0;
 	  flag = OFF;
           isLine = 0;
+	  dataCounter = 0;
           lineCounter++;
+	  printf("\n");
         }
-
     }
   if (errorFlag == TRUE)
     {
       printf("%5d: Error", lineCounter);
     }
 }
-*/
-void cipherSolver(int m, int c, int flag, int b)
+
+int cipherSolver(int m, int c, int flag, int b)
 {
   struct LinearCongruentialGenerator lcg = makeLCG(m,c);
-  int chars;
   int eByte = 0;
-  int dataCounter = 0;
   
-   while((chars = getchar()) != '\n')
-    { 
-      if (chars == ',')
+   while(TRUE)
+    {
+      unsigned long x = getNextRandomValue(&lcg);
+      eByte = x ^ b;
+      if (eByte < 32)
 	{
-	  dataCounter++;
+	  printf("*");
+	  return '@' + eByte;
 	}
-      if (dataCounter == 2 && chars != ',')
+      if (eByte > 32 && eByte <= 127)
 	{
-	  unsigned long x = getNextRandomValue(&lcg);
-	  eByte = x ^ chars;
-	  if (eByte < 32)
-	    {
-	      printf("*");
-	      putchar('@' + eByte);
-	    }
-	  if (eByte > 32 && eByte <= 127)
-	    {
-	      putchar(eByte);
-	    }
+	  return eByte;
 	}
-      }
- }
-
+    }
+}
+    
+    
 
 
 
